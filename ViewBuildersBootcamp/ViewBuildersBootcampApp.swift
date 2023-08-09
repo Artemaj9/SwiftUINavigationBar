e137370dfdc124acc4c3706bba4033f370a5d424
Artem <moiseev@physics.msu.ru>
2023-08-10 01:49:50 +0300

    Initial Commit

---
+++ ViewBuildersBootcamp/ViewBuildersBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBuildersBootcampApp.swift
+//  ViewBuildersBootcamp
+//
+//  Created by Artem on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewBuildersBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
